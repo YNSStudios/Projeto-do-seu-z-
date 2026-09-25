584aa0ce0f7ed20f3c67ae40a390f0fbdc1eee3a
YNSStudios <yurinascimentosouza5@gmail.com>
2026-09-25 11:38:54 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -57,176 +57,63 @@ def consultar_codigo():
                 break
 
         if codigo_encontrado is not None:
-            return codigo_encontrado
+            return devedor
         else:
             print("\033[31mDevedor não encontrado !\033[0m")
             print()
 
-print("========================")
-print()
-print("[1] Cria divida/devedor.")
-print()
-print("[2] Remover/quitar divida.")
-print()
-print("[3] Vizualizar devedores.")
-print()
-print("[4] calcular divida.")
-print()
 
-escolha = verificaçao_numero()
-print()
-
-if escolha == 1:
-
-    print("\033[33mCriação de divida/devedor \033[0m")
-    print()
-
-    nome = input("Digite o nome do devedor: ").title().strip()
-    print()
-
-    print("Digite o valor da divida do devedor. ")
-    divida = verificaçao_numero()
-    print()
-
-    while True:
-
-        print("Digite o nivel de confiança do cliente. ")
-        print()
-        print("[1] cliente duvidoso.")
-        print()
-        print("[2] Cliente novo.")
-        print()
-        print("[3] Cliente de confiança.")
-        print()
-
-        status = verificaçao_numero()
-        print()
-
-        if status == 1:
-
-            credibilidade = "\033[31mCliente duvidoso.\033[0m"
-            credito = 100
-            break
-
-        elif status == 2:
-
-            credibilidade = "\033[33mCliente novato\033[0m"
-            credito = 150
-            break
-
-        elif status == 3:
-
-            credibilidade = "\033[32mCliente de confiança\033[0m"
-            credito = 250
-            break
-
-        else:
-            print("\033[31mOpção inválida !\033[0m")
-            print()
-
-    codigo = cadastrar_codigo
-
-    devedores.append(
-        {
-            "nome": nome,
-            "divida": divida,
-            "credibilidade": credibilidade,
-            "credito": credito,
-            "codigo": codigo,
-        }
-    )
-
-    with open("devedores.json", "w", encoding="utf-8") as dividas:
-        json.dump(devedores, dividas, ensure_ascii=False, indent=5)
-
-    print("\033[32mDevedor cadastrado com sucesso !\033[0m")
-    print()
-
-elif escolha == 2:
-
-    print("\033[33mRemoção/Quitação de divida.\033[0m")
-    print()
+def buscar_codigo(codigo):
 
     for devedor in devedores:
-        print(f"Nome: {devedor['nome']}")
-        print(f"Codigo: {devedor['codigo']}")
-        print(f"Divida: {devedor['divida']}")
+        if devedor["codigo"] == codigo:
+            return devedor
+
+    return None
+
+
+while True:
+
+    print("========================")
+    print()
+    print("[1] Cria divida/devedor.")
+    print()
+    print("[2] Remover/quitar dividas.")
+    print()
+    print("[3] Vizualizar devedores.")
+    print()
+    print("[4] Editar devedores.")
+    print()
+    print("[5] Sair. ")
+    print()
+
+    escolha = verificaçao_numero()
+    print()
+
+    if escolha == 1:
+
+        print("\033[33mCriação de divida/devedor \033[0m")
         print()
-
-    remover_devedor = consultar_codigo()
-
-    devedores.remove(remover_devedor)
-
-    with open("devedores.json", "w", encoding="utf-8") as dividas:
-        json.dump(devedores, dividas, ensure_ascii=False, indent=5)
-
-    print("\033[32mDivida removida/quitada com sucesso.\033[0m")
-    print()
-
-elif escolha == 3:
-
-    print("\033[33mVisualizar devedores.\033[0m")
-    print()
-
-    for devedor in devedores:
-        print("==========================")
-        print(f"Nome: {devedor['nome']}.")
-        print(f"Divida: \033[31mR${devedor['divida']}.\033[0m")
-        print(f"Credibilidade: {devedor['credibilidade']}.")
-        print()
-
-elif escolha == 4:
-
-    print("\033[33mCalcular divida.\033[0m")
-    print()
-
-    for devedor in devedores:
-        print("==========================")
-        print(f"Nome: {devedor['nome']}.")
-        print(f"Divida: \033[31mR${devedor['divida']}.\033[0m")
-        print(f"codigo: {devedor['codigo']}.")
-        print()
-
-    editar_devedor = consultar_codigo()
-
-    print("O que será feito ? ")
-    print()
-    print("[1] Subitrair da divida.")
-    print()
-    print("[2] Soma na divida.")
-    print()
-    print("[3] Status de credibilidade.")
-    print()
-
-    opçao = verificaçao_numero()
-
-    if opçao == 1:
-
-        print("Digite quanto vai ser pago agora. ")
-        print()
-        abate = verificaçao_numero()
-    
-        editar_devedor['divida'] = editar_devedor['divida'] - abate
-
-    elif opçao == 2:
-
-        print("Digite quanto vai ser somado. ")
-        print()
-        soma = verificaçao_numero()
-
-        verificaçao = editar_devedor['divida'] + soma
-
-        if verificaçao > editar_devedor['credibilidade']:
-            print("Não é possivel fazer a soma pois o crédito não é o suficiente.")
-            print()
-        else:
-            editar_devedor['divida'] = verificaçao
-
-    elif opçao == 3:
 
         while True:
-        
-            print("Digite o novo nivel de confiança do cliente. ")
+
+            nome = input("Digite o nome do devedor: ").title().strip()
+            print()
+
+            nome_identico = None
+
+            for devedor in devedores:
+                if devedor['nome'] == nome:
+                    nome_identico = devedor
+
+            if nome_identico is not None:
+                print("\033[33mO nome atual já tem uma replica cadastrada, adicione um sobrenome ou caracterisca a mais para diferenciar. \033[0m")
+            else:
+                break
+
+        while True:
+
+            print("Digite o nivel de confiança do cliente. ")
             print()
             print("[1] cliente duvidoso.")
             print()
@@ -234,39 +121,228 @@ elif escolha == 4:
             print()
             print("[3] Cliente de confiança.")
             print()
-    
+
             status = verificaçao_numero()
             print()
-    
+
             if status == 1:
-    
-                editar_devedor["credibilidade"] = "\033[31mCliente duvidoso.\033[0m"
-                editar_devedor["credito"] = 100
+
+                credibilidade = "\033[31mCliente duvidoso.\033[0m"
+                credito = 100
                 break
-    
+
             elif status == 2:
-    
-                editar_devedor["credibilidade"] = "\033[33mCliente novato\033[0m"
-                editar_devedor["credito"] = 150
+
+                credibilidade = "\033[33mCliente novato\033[0m"
+                credito = 150
                 break
-    
+
             elif status == 3:
-    
-                editar_devedor["credibilidade"] = "\033[32mCliente de confiança\033[0m"
-                editar_devedor["credito"] = 250
+
+                credibilidade = "\033[32mCliente de confiança\033[0m"
+                credito = 250
                 break
-    
+
             else:
                 print("\033[31mOpção inválida !\033[0m")
                 print()
 
-    else:
-        print("\033[31mOpção inválida !\033[0m")
+        divida = 0
+
+        codigo = cadastrar_codigo()
+
+        print(f"Nome: {nome}")
+        print(f"Codigo: {codigo}")
         print()
 
-    devedores.append(editar_devedor)
+        devedores.append(
+            {
+                "nome": nome,
+                "divida": divida,
+                "credibilidade": credibilidade,
+                "credito": credito,
+                "codigo": codigo,
+            }
+        )
 
-    with open("devedores.json", 'w', encoding="utf-8") as dividas:
-        json.dump(devedores, dividas, ensure_ascii=False, indent=5)
+        with open("devedores.json", "w", encoding="utf-8") as dividas:
+            json.dump(devedores, dividas, ensure_ascii=False, indent=5)
 
-    print("\033[32mEdição comcluida com sucesso !\033[0m")
+        devedor = buscar_codigo(codigo)
+
+        print("Digite o valor da divida. ")
+        divida_1 = verificaçao_numero()
+        print()
+
+        if divida_1 > devedor['credito']:
+            print("\033[31mCuidado, o cliente já tem uma divida maior que o credito padrão.\033[0m Você pode alterar a credibilidade na função 4.")
+
+        devedor['divida'] += divida_1
+
+        with open("devedores.json", "w", encoding="utf-8") as dividas:
+            json.dump(devedores, dividas, ensure_ascii=False, indent=5)
+
+        print("\033[32mDevedor cadastrado com sucesso !\033[0m")
+        print()
+
+    elif escolha == 2:
+
+        print("\033[33mRemoção/Quitação de divida.\033[0m")
+        print()
+
+        for devedor in devedores:
+            print(f"Nome: {devedor['nome']}")
+            print(f"Codigo: {devedor['codigo']}")
+            print(f"Divida: {devedor['divida']}")
+            print()
+
+        remover_devedor = consultar_codigo()
+
+        devedores.remove(remover_devedor)
+
+        with open("devedores.json", "w", encoding="utf-8") as dividas:
+            json.dump(devedores, dividas, ensure_ascii=False, indent=5)
+
+        print("\033[32mDivida removida/quitada com sucesso.\033[0m")
+        print()
+
+    elif escolha == 3:
+
+        print("\033[33mVisualizar devedores.\033[0m")
+        print()
+
+        for devedor in devedores:
+            print("==========================")
+            print(f"Nome: {devedor['nome']}.")
+            print(f"Divida: \033[31mR${devedor['divida']}.\033[0m")
+            print(f"Credibilidade: {devedor['credibilidade']}.")
+            print()
+
+    elif escolha == 4:
+
+        print("\033[33mEditar devedores.\033[0m")
+        print()
+
+        for devedor in devedores:
+            print("==========================")
+            print(f"Nome: {devedor['nome']}.")
+            print(f"Divida: \033[31mR${devedor['divida']}.\033[0m")
+            print(f"codigo: {devedor['codigo']}.")
+            print()
+
+        editar_devedor = consultar_codigo()
+
+        print("O que será feito ? ")
+        print()
+        print("[1] Subitrair da divida.")
+        print()
+        print("[2] Soma na divida.")
+        print()
+        print("[3] Status de credibilidade.")
+        print()
+
+        while True:
+
+            opçao = verificaçao_numero()
+
+            if opçao == 1:
+
+                print("Digite quanto vai ser pago agora. ")
+                print()
+
+                abate = verificaçao_numero()
+
+                editar_devedor["divida"] = editar_devedor["divida"] - abate
+
+                if editar_devedor["divida"] < 0:
+
+                    troco = editar_devedor["divida"] * -1
+
+                    print(f"O troco será de R${troco}")
+                    print()
+
+                    editar_devedor["divida"] = 0
+
+                break
+
+            elif opçao == 2:
+
+                print("Digite quanto vai ser somado. ")
+                print()
+
+                soma = verificaçao_numero()
+
+                verificaçao = editar_devedor["divida"] + soma
+
+                if verificaçao > editar_devedor["credito"] + 20:
+
+                    print("Não é possivel fazer a soma pois o crédito não é o suficiente. É nescessario alterar a credibilidade. ")
+                    print()
+
+                else:
+
+                    editar_devedor["divida"] = verificaçao
+                    if verificaçao > editar_devedor['credito']:
+                        print("\033[33mAvise ao cliente que o crédito acabou !\033[0m")
+                    break
+
+            elif opçao == 3:
+
+                while True:
+
+                    print("Digite o novo nivel de confiança do cliente. ")
+                    print()
+
+                    print("[1] cliente duvidoso.")
+                    print()
+                    print("[2] Cliente novo.")
+                    print()
+                    print("[3] Cliente de confiança.")
+                    print()
+
+                    status = verificaçao_numero()
+                    print()
+
+                    if status == 1:
+
+                        editar_devedor["credibilidade"] = "\033[31mCliente duvidoso.\033[0m"
+                        editar_devedor["credito"] = 100
+                        break
+
+                    elif status == 2:
+
+                        editar_devedor["credibilidade"] = "\033[33mCliente novato\033[0m"
+                        editar_devedor["credito"] = 150
+                        break
+
+                    elif status == 3:
+
+                        editar_devedor["credibilidade"] = "\033[32mCliente de confiança\033[0m"
+                        editar_devedor["credito"] = 250
+                        break
+
+                    else:
+
+                        print("\033[31mOpção inválida !\033[0m")
+                        print()
+
+                break
+
+            else:
+
+                print("\033[31mOpção inválida !\033[0m")
+                print()
+
+
+        with open("devedores.json", "w", encoding="utf-8") as dividas:
+            json.dump(devedores, dividas, ensure_ascii=False, indent=5)
+
+        print("\033[32mEdição concluida com sucesso !\033[0m")
+        print()
+
+    elif escolha == 5:
+        print("\033[31mSaindo ....\033[0m")
+        for i in range(random.randint(3, 5)):
+            print("\033[33m...\033[0m")
+            sleep(0.6)
+        break
